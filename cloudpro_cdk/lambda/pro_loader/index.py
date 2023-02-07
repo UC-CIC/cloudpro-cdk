33ab208b2af5ad30c311cc10b8754c802acc5b1e
Chris Robinson <awscrob@amazon.com>
2023-02-06 22:57:21 -0800

    Explore suffix + prefix filtering combo.  Only prefix filter supported.  Implemented.

--- cloudpro_cdk/lambda/pro_loader/index.py
+++ cloudpro_cdk/lambda/pro_loader/index.py
@@ -3,6 +3,8 @@ import json
 def handler(event,context):
     # TODO implement
     print("<<< LOG ME >>>")
+    print(event)
+    print("<<< LOG ME >>>")
     return {
         'statusCode': 200,
         'body': json.dumps('Hello 2 from Lambda!')
--- cloudpro_cdk/propack_loader.py
+++ cloudpro_cdk/propack_loader.py
@@ -9,14 +9,29 @@ from aws_cdk import(
     aws_lambda as lambda_,
     aws_events as events,
     aws_sqs as sqs,
-    aws_events_targets as targets
+    aws_events_targets as targets,
+    aws_dynamodb as dynamodb
 )
 
 class ProPackLoaderStack(Stack):
     def __init__(self,scope: Construct,  construct_id: str, pro_bus:events.EventBus, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        propacks_bucket=s3.Bucket(self, "cdk-bucket-propack",
+
+        
+        dynamo_propack_questionnaire = dynamodb.Table(self,"cdk-dynamo-propack-questionnaire",
+            partition_key=dynamodb.Attribute(name="pro_pack", type=dynamodb.AttributeType.STRING),
+            #sort_key=dynamodb.Attribute(name="lorem", type=dynamodb.AttributeType.STRING),
+            billing_mode=dynamodb.BillingMode.PAY_PER_REQUEST
+        )
+        dynamo_propack_scoring = dynamodb.Table(self,"cdk-dynamo-propack-scoring",
+            partition_key=dynamodb.Attribute(name="pro_pack", type=dynamodb.AttributeType.STRING),
+            #sort_key=dynamodb.Attribute(name="lorem", type=dynamodb.AttributeType.STRING),
+            billing_mode=dynamodb.BillingMode.PAY_PER_REQUEST
+        )
+        
+
+        bucket_propack=s3.Bucket(self, "cdk-bucket-propack",
             versioned=True,
             removal_policy=RemovalPolicy.DESTROY,
             auto_delete_objects=True,
@@ -28,43 +43,56 @@ class ProPackLoaderStack(Stack):
             self,"cdk-fn-propack-loader",
             runtime=lambda_.Runtime.PYTHON_3_9,
             handler="index.handler",
-            code=lambda_.Code.from_asset(os.path.join("cloudpro_cdk/lambda/","pro_loader"))
+            code=lambda_.Code.from_asset(os.path.join("cloudpro_cdk/lambda/","pro_loader")),
+            environment={
+                "TABLE_QUESTIONNAIRE":dynamo_propack_questionnaire.table_name,
+                "TABLE_SCORING":dynamo_propack_scoring.table_name
+            }
         )
 
 
         # s3.EventType.OBJECT_CREATED
-        propack_rule = events.Rule(self, "cdk-rule-propack-new",
+        rule_propack_new = events.Rule(self, "cdk-rule-propack-new",
             description="Rule for when new PRO packs are uploaded to S3.",
             event_pattern=events.EventPattern(
                 source=["aws.s3"],
                 detail_type=["Object Created"],
-                detail={"bucket":{"name":[ propacks_bucket.bucket_name ]}}
+                detail={"bucket":{"name":[ bucket_propack.bucket_name ]}}
             ),
             event_bus=pro_bus
         )
 
-        dflt_propack_dlq = sqs.Queue(self, "cdk-sqs-dflt-propack-dlq")
+        sqs_dflt_propack_dlq = sqs.Queue(self, "cdk-sqs-dflt-propack-dlq")
         # fwd to app bus
-        dflt_fwd_propack_new_rule = events.Rule(self, "cdk-fwd-propack-new",
+        rule_dflt_fwd_propack_newe = events.Rule(self, "cdk-rule-dflt-fwd-propack-new",
             description="Rule to forward from default bus.",
             event_pattern=events.EventPattern(
                 source=["aws.s3"],
                 detail_type=["Object Created"],
-                detail={"bucket":{"name":[ propacks_bucket.bucket_name ]}}
-            )
-        )
-        dflt_fwd_propack_new_rule.add_target(
-            targets.EventBus(
-                pro_bus,
-                dead_letter_queue=dflt_propack_dlq
+                detail={
+                    "bucket": {
+                        "name":[ bucket_propack.bucket_name ]
+                    },
+                    "object": {
+                        "key": [ { "prefix": "raw/" }]    
+                    }
+                }
             )
         )
 
-        propack_dlq = sqs.Queue(self, "cdk-sqs-propack-dlq")
-        propack_rule.add_target(
+
+        rule_dflt_fwd_propack_newe.add_target(
+            targets.EventBus(
+                pro_bus,
+                dead_letter_queue=sqs_dflt_propack_dlq
+            )
+        )
+
+        sqs_propack_dlq = sqs.Queue(self, "cdk-sqs-propack-dlq")
+        rule_propack_new.add_target(
             targets.LambdaFunction(
                 fn_propack_loader,
-                dead_letter_queue=propack_dlq,
+                dead_letter_queue=sqs_propack_dlq,
                 max_event_age=Duration.hours(2), 
                 retry_attempts=2
             )
