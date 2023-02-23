b9dc7c9e06ab67d5a59d715df5cf34533b507ce6
Chris Robinson <awscrob@amazon.com>
2023-02-22 21:48:17 -0800

    Minor tweaks. Foundations are now set for user profile initiation (and staged profile).

--- cloudpro_cdk/lambda/cognito/cognito_post_confirmation/index.py
+++ cloudpro_cdk/lambda/cognito/cognito_post_confirmation/index.py
@@ -1,9 +1,33 @@
 import json
 import boto3
 import os
+import botocore.exceptions
 
+dynamodb = boto3.resource('dynamodb')
+table_name_user=os.environ["TABLE_USER"]
 
+#event.request.userAttributes.email
 
+def init_user_record( contact:str ):
+    table = dynamodb.Table(table_name_user)
+
+    user_payload = {
+        'email':contact,
+        'state':"INIT"
+    }
+    
+    try:
+        table.put_item(
+            Item=user_payload,
+            ConditionExpression='attribute_not_exists(email)'
+        )
+    except botocore.exceptions.ClientError as e:
+        # Ignore the ConditionalCheckFailedException, bubble up
+        # other exceptions.
+        if e.response['Error']['Code'] != 'ConditionalCheckFailedException':
+            raise
+        else:
+            print("<Existing Staged Record>")
 
 def handler(event, context):
     """Post Confirmation
@@ -11,6 +35,14 @@ def handler(event, context):
     """
     print(event, '--------- Start --------------')
     
+    request = event.get('request')
+    contact = request.get('userAttributes').get('email')
+    #print("EMAIL: ", contact)
+
+    try:
+        init_user_record( contact ) 
+    except Exception as e:
+        print(e)
 
     print(event, "-----end---------")
    
