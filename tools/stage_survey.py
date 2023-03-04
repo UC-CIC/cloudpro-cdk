17ae9e20e367f872324eca20da57eda19ea5cd6a
Chris Robinson <awscrob@amazon.com>
2023-03-04 14:39:17 -0800

    Testing flow of multiple sections

--- tools/stage_survey.py
+++ tools/stage_survey.py
@@ -19,38 +19,128 @@ table_name=SURVEY_TABLE
 payload = {
     "sub":"85a163cc-3d8f-4918-b3e3-d296ed6710ae",
     "completed_surveys":[
+        { "yyyyPre Operation": [
+                {
+                    "sid":hashlib.sha256( (SUB+SURVEYS[0]).encode('utf-8') ).hexdigest(),
+                    "name": "Mobility",
+                    "description": "Lorem MOBILITY ipsum  dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                    "assigned":"2023-03-01",
+                    "due":"2023-03-10",
+                    "completed":"",
+                    "missed":""
+                },
+                {
+                    "sid":hashlib.sha256( (SUB+SURVEYS[1]).encode('utf-8') ).hexdigest(),
+                    "name": "Physical Function",
+                    "description": "Lorem PHYSICAL FUNCTION ipsum  dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                    "assigned":"2023-03-01",
+                    "due":"2023-03-10",
+                    "completed":"",
+                    "missed":""
+                },
+                {
+                    "sid":hashlib.sha256( (SUB+SURVEYS[2]).encode('utf-8') ).hexdigest(),
+                    "name": "Upper Extrimity",
+                    "description": "Lorem UPPER EXTREMITY ipsum  dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                    "assigned":"2023-03-01",
+                    "due":"2023-03-10",
+                    "completed":"",
+                    "missed":""
+                }
+            ] 
+        },
+                { "zzzzzzPre Operation": [
+                {
+                    "sid":hashlib.sha256( (SUB+SURVEYS[0]).encode('utf-8') ).hexdigest(),
+                    "name": "Mobility",
+                    "description": "Lorem MOBILITY ipsum  dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                    "assigned":"2023-03-01",
+                    "due":"2023-03-10",
+                    "completed":"",
+                    "missed":""
+                },
+                {
+                    "sid":hashlib.sha256( (SUB+SURVEYS[1]).encode('utf-8') ).hexdigest(),
+                    "name": "Physical Function",
+                    "description": "Lorem PHYSICAL FUNCTION ipsum  dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                    "assigned":"2023-03-01",
+                    "due":"2023-03-10",
+                    "completed":"",
+                    "missed":""
+                },
+                {
+                    "sid":hashlib.sha256( (SUB+SURVEYS[2]).encode('utf-8') ).hexdigest(),
+                    "name": "Upper Extrimity",
+                    "description": "Lorem UPPER EXTREMITY ipsum  dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                    "assigned":"2023-03-01",
+                    "due":"2023-03-10",
+                    "completed":"",
+                    "missed":""
+                }
+            ] 
+        }    
     ],
     "open_surveys":[
-        {
-            "sid":hashlib.sha256( (SUB+SURVEYS[0]).encode('utf-8') ).hexdigest(),
-            "name": "Mobility",
-            "description": "Lorem MOBILITY ipsum  dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-            "assigned":"2023-03-01",
-            "due":"2023-03-10",
-            "completed":"",
-            "missed":""
+        { "Pre Operation": [
+                {
+                    "sid":hashlib.sha256( (SUB+SURVEYS[0]).encode('utf-8') ).hexdigest(),
+                    "name": "Mobility",
+                    "description": "Lorem MOBILITY ipsum  dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                    "assigned":"2023-03-01",
+                    "due":"2023-03-10",
+                    "completed":"",
+                    "missed":""
+                },
+                {
+                    "sid":hashlib.sha256( (SUB+SURVEYS[1]).encode('utf-8') ).hexdigest(),
+                    "name": "Physical Function",
+                    "description": "Lorem PHYSICAL FUNCTION ipsum  dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                    "assigned":"2023-03-01",
+                    "due":"2023-03-10",
+                    "completed":"",
+                    "missed":""
+                },
+                {
+                    "sid":hashlib.sha256( (SUB+SURVEYS[2]).encode('utf-8') ).hexdigest(),
+                    "name": "Upper Extrimity",
+                    "description": "Lorem UPPER EXTREMITY ipsum  dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                    "assigned":"2023-03-01",
+                    "due":"2023-03-10",
+                    "completed":"",
+                    "missed":""
+                }
+            ] 
         },
-        {
-            "sid":hashlib.sha256( (SUB+SURVEYS[1]).encode('utf-8') ).hexdigest(),
-            "name": "Physical Function",
-            "description": "Lorem PHYSICAL FUNCTION ipsum  dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-            "assigned":"2023-03-01",
-            "due":"2023-03-10",
-            "completed":"",
-            "missed":""
-        },
-        {
-            "sid":hashlib.sha256( (SUB+SURVEYS[2]).encode('utf-8') ).hexdigest(),
-            "name": "Upper Extrimity",
-            "description": "Lorem UPPER EXTREMITY ipsum  dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-            "assigned":"2023-03-01",
-            "due":"2023-03-10",
-            "completed":"",
-            "missed":""
+                { "xxxPre Operation": [
+                {
+                    "sid":hashlib.sha256( (SUB+SURVEYS[0]).encode('utf-8') ).hexdigest(),
+                    "name": "Mobility",
+                    "description": "Lorem MOBILITY ipsum  dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                    "assigned":"2023-03-01",
+                    "due":"2023-03-10",
+                    "completed":"",
+                    "missed":""
+                },
+                {
+                    "sid":hashlib.sha256( (SUB+SURVEYS[1]).encode('utf-8') ).hexdigest(),
+                    "name": "Physical Function",
+                    "description": "Lorem PHYSICAL FUNCTION ipsum  dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                    "assigned":"2023-03-01",
+                    "due":"2023-03-10",
+                    "completed":"",
+                    "missed":""
+                },
+                {
+                    "sid":hashlib.sha256( (SUB+SURVEYS[2]).encode('utf-8') ).hexdigest(),
+                    "name": "Upper Extrimity",
+                    "description": "Lorem UPPER EXTREMITY ipsum  dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                    "assigned":"2023-03-01",
+                    "due":"2023-03-10",
+                    "completed":"",
+                    "missed":""
+                }
+            ] 
         }
-    ],
-    "missed_surveys":[
-    
     ]
 }
 
