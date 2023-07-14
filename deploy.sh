da7f1815a53709ddaebb3aa9ca5acd29376d27f7
Chris Robinson <awscrob@amazon.com>
2023-07-14 10:13:54 -0700

    Fix dropped line

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 echo ~~Start Processing~~
 
-#layer_arn="$(python3 layers_get_latest.py layer_cloudpro_lib)"
+layer_arn="$(python3 layers_get_latest.py layer_cloudpro_lib)"
 for var in "$@"
 do
     cdk deploy $var --context layer_arn=$layer_arn --context XKEY=DUMMY --context debug_token=DummyDebug5568dd5ea5fb41d082ff154b4b8336338b47460173358288f57a6cdd2230dccc
